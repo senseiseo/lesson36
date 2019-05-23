405a8e0b645ecc46ca4241148fe527d609a26378
ildar <1919mur@gmail.com>
2019-05-23 19:38:11 +0300

    //display validations error

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get 'home/index'
 
+
+  resource :contacts, only: [:new, :create], path_names: { :new => '' }
   resources :articles
-  resources :contacts, only: [:new, :create]
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
