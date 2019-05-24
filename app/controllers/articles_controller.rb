b05762bb75b637c10cf333ec278328b5bde5d8d2
ildar <1919mur@gmail.com>
2019-05-24 18:20:21 +0300

    //add edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 
     def index
         @articles = Article.all
-      end
+    end
     
     def show
         @article = Article.find(params[:id])
@@ -13,14 +13,26 @@ class ArticlesController < ApplicationController
   
     def create
       @article = Article.new(article_params)
-      if @article.valid?
-        @article.save
+      if @article.save
         redirect_to @article
       else
         render action: 'new'
       end
     end
   
+    def edit 
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to @article
+          else
+            render action: 'edit'
+          end
+    end
+
     private
   
     def article_params
