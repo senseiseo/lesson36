6f41bf145897756dd2c1220788874bf7fd517950
ildar <1919mur@gmail.com>
2019-05-26 21:57:13 +0300

    //need repear

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,19 @@
+class CommentsController < ApplicationController
+    def create 
+        @article = Article.find(params[:article_id])
+        @article.comment.create(comment_params)
+        
+        redirect_to article_path(@article)
+    end
+
+    private 
+
+    def comment_params
+        params.require(:comment).permit(:author, :body)
+        
+    end
+    
+
+
+end
+ 
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,10 @@
 class PagesController < ApplicationController
+
+    def terms 
+    end
+
+    def about 
+    end
+    
+
 end
