5f8a862ddf735bd6a5c572ee94e62c2710d9ce05
ildar <1919mur@gmail.com>
2019-06-02 13:22:05 +0300

    //before action add

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_30_114441) do
+ActiveRecord::Schema.define(version: 2019_06_02_081353) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_05_30_114441) do
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
